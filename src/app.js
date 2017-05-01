40c6beed7a4bbb7ee65895302f93273e383aa7db
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-05-01 19:08:44 +0300

    modifications to files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
-const app = express()
 const path = require('path')
-
+const exphbs = require('express-handlebars')
 const router = require('./router.js')
+
+const app = express()
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'default',
+  // layoutsDir: path.join(__dirname, "views", "layouts"),
+  extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
+
+app.set('views', path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, '..','public')));
 
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const helloController = require('./controllers/hello.js')
+const resultController = require('./controllers/result.js')
 
 router.get('/hello', helloController)
+router.get('/result', resultController)
 
 module.exports = router
