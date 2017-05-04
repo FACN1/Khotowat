5d3369d77df23615ab871fb124a7fb477b3de195
Reem Qais <reemq86@hotmail.com>
2017-05-04 11:36:58 +0300

    Add a tag href to button input elements

--- src/controllers/result.js
+++ src/controllers/result.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.render('result')
+module.exports = (req, res, next) => {
+  res.render('result', { title : 'إنهاء'})
 }
--- src/router.js
+++ src/router.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const homeController = require('./controllers/home.js');
 const questionsController = require('./controllers/questions.js');
 const finishController = require('./controllers/finish.js');
+const resultController = require('./controllers/result.js');
 
 router.get('/', homeController);
 router.get('/questions', questionsController);
 router.get('/finish', finishController)
+router.get('/result', resultController)
 
 module.exports = router
